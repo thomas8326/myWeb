96b4c7e4200ac5c41629e1c0664f0321d8628178
Thomas shih <tt7919@gmail.com>
2020-12-29 22:42:02 +0800

    chore: update eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  ignorePatterns: ['.eslintrc.js'],
 };
