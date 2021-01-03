234441700cdc11b0556fcd8b8c9b69714945c103
Thomas shih <tt7919@gmail.com>
2021-01-03 21:05:10 +0800

    refactor: rename

--- src/containers/showroom/showroom.tsx
+++ src/containers/showroom/showroom.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ShowroomHeader from 'src/components/header';
+import { Popover, Button } from 'antd';
 
 function Showroom() {
   return (
     <>
       <ShowroomHeader />
+      <Popover title="Message" trigger="click">
+        <Button>Chat</Button>
+      </Popover>
       <div className="full-layout flex-col flex-center"> is Showroom</div>
     </>
   );
