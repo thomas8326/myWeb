c20d5acf14941fae169fe872997303c29a681d82
thomas shih <tt7919@gmail.com>
2021-03-08 13:07:05 +0800

    feat: update token a list

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,4 +2,4 @@
 export const ONE_DAY_CLOCK = 24;
 export const HALF_OF_HOUR = 30;
 export const ONE_HOUR = 60;
-export const GIT_ACCESS_TOKEN = '37302555674d2c8bd34bc5a0fc52b33087339dd2';
+export const GIT_ACCESS_TOKEN = '7534fd2deb10d7c10175b848f99087f24b7cb609';
--- src/containers/github-repo/github-repo-list.tsx
+++ src/containers/github-repo/github-repo-list.tsx
@@ -48,10 +48,6 @@ export default function GithubRepoList(props: InfiniteScrollProps<{ searchName: 
         },
       })
       .then((response) => {
-        if (response.status !== 204) {
-          return;
-        }
-
         if (isLoadCompleted && !response.data.items.length) {
           isLoadCompleted(true);
           return;
