641a33fa781d2d83e27b8189a72069c9e8fd6eb9
Thomas shih <tt7919@gmail.com>
2022-06-19 19:03:43 +0800

    feat: add new trans keys

--- src/enums/translation-key.enum.ts
+++ src/enums/translation-key.enum.ts
@@ -2,4 +2,7 @@ export enum TransKey {
     Resume = 'resume',
     Projects = 'projects',
     Components = 'components',
+    BasicInfo = 'basic info',
+    WorkExperience = 'work experience',
+
 }
