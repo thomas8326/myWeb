84ac8c0f2fb6dbb7c9422fa141495661e6393304
Thomas shih <tt7919@gmail.com>
2022-06-25 18:54:34 +0800

    feat: move context provider in to context folder

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ModalContext } from "src/components/dialogs/modal";
+import { ModalContext } from "src/contexts/modal";
 
 const useModal = () => {
     const context = useContext(ModalContext);
