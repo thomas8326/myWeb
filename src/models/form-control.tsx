bf6f768dbf9a5e2088a9df984acb7f11fa2d58d6
Thomas shih <tt7919@gmail.com>
2022-07-19 20:56:10 +0800

    feat: add formControl entity

---
+++ src/models/form-control.tsx
@@ -0,0 +1,40 @@
+export class FormControl<T> {
+    value: T;
+    validations: { (value: string): { name: string; valid: boolean } }[];
+    error: { [key: string]: boolean } = {};
+    valid: boolean = false;
+    isDirty: boolean = false;
+
+    constructor(value: T, validations?: { (value: any): { name: string; valid: boolean } }[]) {
+        this.value = value;
+        this.validations = validations || [];
+    }
+
+    setValue(value: T) {
+        this.value = value;
+    }
+
+    getValue() {
+        return this.value;
+    }
+
+    setError(name: string, isError: boolean) {
+        this.error[name] = isError;
+    }
+
+    hasError(name: string) {
+        return this.error[name];
+    }
+
+    isError() {
+        return Object.values(this.error).some((value) => !!value);
+    }
+
+    setDirty() {
+        this.isDirty = true;
+    }
+
+    getIsDirty() {
+        return this.isDirty;
+    }
+}
