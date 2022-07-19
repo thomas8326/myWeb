05afba7319a6158baabb0cdbf89b90d3419672d2
Thomas shih <tt7919@gmail.com>
2022-07-19 21:37:05 +0800

    feat: add firebase config.

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { ref, getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC5iy3Yy-N-FkQsz0CGpX7ObEL7rLfEbuk",
+    authDomain: "developer-thoams.firebaseapp.com",
+    databaseURL: "https://developer-thoams-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "developer-thoams",
+    storageBucket: "developer-thoams.appspot.com",
+    messagingSenderId: "298894432266",
+    appId: "1:298894432266:web:0197139a28c3ad375167f3",
+    measurementId: "G-RQB3895PK2"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export const realtimeDB = getDatabase(firebase);
+export const dbref = (type: 'value' | 'list') => ref(realtimeDB, type);
